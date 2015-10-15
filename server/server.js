6c0aff7a4ab3644251ed373c6795a6d2e56dc717
Kristin <coldzonemt@yahoo.com>
2015-10-15 16:44:50 -0700

    (feat) implement server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-var express = require()
+var express = require('../node_modules/express');
+var parser = require('../node_modules/body-parser');
+var db = require('./db.js');
+
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.use(parser.json());
+
+app.use(express.static('client'));
+
+//Routes go here
+app.get('/', function(req, res) {
+  res.send('helllllooooooooo woooorld! :D');
+});
+
+app.listen(port, function() {
+  console.log('Listening on port: ', port);
+});
+
+exports.app = app; 
