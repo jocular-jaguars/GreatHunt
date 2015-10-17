5ecd5a7ed2f9bbc8fb78ab024780259adc5b94e0
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-10-17 16:51:44 -0700

    Added framework for client-side unit tests

---
+++ client/www/tests/controllersTest.js
@@ -0,0 +1,15 @@
+
+//enter "karma start unit-tests.conf.js" in tests folder to run this!
+
+describe('welcomeCtrl', function() {
+
+  describe('#addingForNoReason', function() {
+    var bool = true;
+
+      it('should add two numbers', function() {
+        // expect(welcomeCtrl.addingForNoReason(1,2)).to.equal(3);
+        expect(bool).toBe(true);
+      });
+
+  });
+})
