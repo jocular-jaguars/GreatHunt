1c8824c63467d7fd36e91df8a76bfe3dfef5598e
rfenolio <mordoc@gmail.com>
2015-10-19 16:04:28 -0700

    (feat) write Team class tests

---
+++ server/tests/teamTest.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var Team = require('../game/Team.js');
+
+
+
+describe('Team ', function() {
+  beforeEach(function(done) {
+    team = new Team({
+      name: 'teamName',
+      currentChallenge; 0 
+    });
+    done();
+  });
+
+  it('should have a nextChallenge function', function(done) {
+    expect(team.nextChallenge).to.exist;
+    done();
+  });
+
+  it('should increment the current challenge', function(done) {
+    team.nextChallenge();
+    team.nextChallenge();
+    expect(team.currentChallenge).to.equal(2);
+    done();
+  });
+});
