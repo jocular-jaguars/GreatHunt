1539aedd0f14d08e4643adc5de5b9b92ea1ed88e
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-10-19 23:43:57 -0700

    Add factories to handle $resource requests for server

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -1,10 +1,28 @@
-angular.module('app.services', [])
+angular.module('app.services', ['ngResource'])
 
-.factory('BlankFactory', [function(){
-
+.factory('game', [function($resource){
+  return $resource(
+    'api/game:id',
+    {id: '@id'},
+    {
+      'update' : {method: 'PUT'},
+      'create' : {method: 'POST'}
+    }
+  );
 }])
 
-.service('BlankService', [function(){
+.factory('team', [function($resource){
+  return $resource(
+    'api/team:id',
+    {id: '@id'},
+    {
+      'make' : {method: 'POST'}
+    }
+  );
+}])
 
+.factory('hunts', [function($resource){
+  return $resource(
+    'api/hunts'
+  );
 }]);
-
