ad32b13d97d3950315458dd24a781793363d345b
Angel Tam <angeltam@gmail.com>
2015-10-20 16:05:50 -0700

    Code cleanup of ionic router, controllers, and template files

--- client/www/js/routes.js
+++ client/www/js/routes.js
@@ -10,15 +10,15 @@ angular.module('app.routes', [])
   $stateProvider
 
     .state('tabs', {
-      url: '/page10',
+      url: '/home',
       abstract:true,
       templateUrl: 'templates/tabs.html'
     })
 
     .state('tabs.welcome', {
-      url: '/page1',
+      url: '/welcome',
       views: {
-        'tab1': {
+        'welcome': {
           templateUrl: 'templates/welcome.html',
           controller: 'welcomeCtrl'
         }
@@ -26,9 +26,9 @@ angular.module('app.routes', [])
     })
 
     .state('tabs.about', {
-      url: '/page14',
+      url: '/about',
       views: {
-        'tab2': {
+        'about': {
           templateUrl: 'templates/about.html',
           controller: 'aboutCtrl'
         }
@@ -36,54 +36,54 @@ angular.module('app.routes', [])
     })
 
     .state('lobby', {
-      url: '/page2',
+      url: '/lobby',
       templateUrl: 'templates/lobby.html',
       controller: 'lobbyCtrl'
     })
 
-    .state('creatorLobby', {
-      url: '/page3',
-      templateUrl: 'templates/creatorLobby.html',
-      controller: 'creatorLobbyCtrl'
+    .state('creatorJoin', {
+      url: '/creatorjoin',
+      templateUrl: 'templates/creatorJoin.html',
+      controller: 'creatorJoinCtrl'
     })
 
-    .state('creatorParticipation', {
-      url: '/page4',
-      templateUrl: 'templates/creatorParticipation.html',
-      controller: 'creatorParticipationCtrl'
-    })
-
-    .state('teamForm', {
-      url: '/page5',
-      templateUrl: 'templates/teamForm.html',
-      controller: 'teamFormCtrl'
+    .state('createTeam', {
+      url: '/createteam',
+      templateUrl: 'templates/createTeam.html',
+      controller: 'createTeamCtrl'
     })
 
     .state('challenge', {
-      url: '/page6',
+      url: '/challenge',
       templateUrl: 'templates/challenge.html',
       controller: 'challengeCtrl'
     })
 
-    .state('challengeCorrect', {
-      url: '/page7',
-      templateUrl: 'templates/challengeCorrect.html',
-      controller: 'challengeCorrectCtrl'
-    })
-
-    .state('challengeIncorrect', {
-      url: '/page8',
-      templateUrl: 'templates/challengeIncorrect.html',
-      controller: 'challengeIncorrectCtrl'
-    })
-
     .state('endGame', {
-      url: '/page9',
+      url: '/end',
       templateUrl: 'templates/endGame.html',
       controller: 'endGameCtrl'
+    })
+
+    .state('dashboard', {
+      url: '/dashboard',
+      templateUrl: 'templates/dashboard.html',
+      controller: 'dashboardCtrl'
+    })
+
+    .state('correct', {
+      url: '/correct',
+      templateUrl: 'templates/correct.html',
+      controller: 'correctCtrl'
+    })
+
+    .state('incorrect', {
+      url: '/incorrect',
+      templateUrl: 'templates/incorrect.html',
+      controller: 'incorrectCtrl'
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/page10/page1');
+  $urlRouterProvider.otherwise('/home/welcome');
 
 });
