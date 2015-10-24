fd149ae6386de7cd872f554d8d46d8f0d6343c5e
Kristin <coldzonemt@yahoo.com>
2015-10-24 15:01:10 -0700

    set up backend routing before continuing with form

--- client/www/huntCreation/js/controllers.js
+++ client/www/huntCreation/js/controllers.js
@@ -1,18 +1,20 @@
 angular.module('app.controllers', [])
-  
+
 .controller('createChallengeCtrl', function($scope) {
 
 })
-   
+
 .controller('createHuntCtrl', function($scope) {
+  $scope.createFirstChallenge = function(hunt, challengeId) {
+
+  }
 
 })
-   
+
 .controller('previewChallengeCtrl', function($scope) {
 
 })
-   
+
 .controller('previewHuntCtrl', function($scope) {
 
 })
- 
