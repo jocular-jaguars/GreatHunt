60fc92a920838b022d3a438e9bc24b48a1394236
Kristin <coldzonemt@yahoo.com>
2015-10-24 11:45:44 -0700

    pre-rebase commit

--- client/www/huntCreation/js/services.js
+++ client/www/huntCreation/js/services.js
@@ -12,15 +12,34 @@ angular.module('app.services', [])
 
   var hunts = data.query();
 
+  var addChallengeToHunt = function() {
+
+  };
+
+  var addHunt = function() {
+    //post method TODO: add new backend api route
+    //TODO: use $resource, and if I use $resource,
+    //I must use $promise.then(...) for promises
+      return $http({
+        method: 'POST',
+        url: '/api/hunts',
+        data: project
+      })
+      .then(function (resp) {
+        //TODO: figure out the path here.
+        //Also, use $state.go('/pathName');
+        $location.path('/hunts');
+        return resp;
+      })
+    }
+  };
+
   return {
     getHunts: function() {
       return hunts;
     },
     getHunt: function(index) {
       return hunts[index];
-    }
-  }
-
-.service('BlankService', [function(){
-
-}]);
+    },
+    addHunt: addHunt
+  });
