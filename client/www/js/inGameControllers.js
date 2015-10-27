3d9e06fc8b8c8762b0cc7cf4daf9d9abbfb7c433
Angel Tam <angeltam@gmail.com>
2015-10-26 17:15:14 -0700

    Client-side validate when user enters answer to challenge question - Disable submit button until answer is correct - Instantiate - TODO: check answer against game object on localstorage

--- client/www/js/inGameControllers.js
+++ client/www/js/inGameControllers.js
@@ -5,6 +5,18 @@ angular.module('app.inGameControllers', ['app.services', 'ngResource'])
   //fake data until we've made server call
   $scope.description = "Don't forget your towel!";
   $scope.question = '123';
+  $scope.correctAnswer = 'hello';  // for testing. use localscope game object
+  $scope.current = {};
+  $scope.current.input;
+
+  $scope.submitAnswer = function() {
+    console.log("submitted my answer to the server!");
+  }
+
+  // Check if user input answer is wrong
+  $scope.isWrong = function() {
+    return ($scope.correctAnswer !== $scope.current.input);
+  }
 
 })
 
